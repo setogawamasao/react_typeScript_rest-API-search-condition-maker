b43c5c71de4a8e9728e50a81615101b65b10f18d
masaosetogawa <ms.job.hunting@gmail.com>
2020-04-05 21:49:31 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IState {
+  conditions: {};
+}
+
+class App extends React.Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { conditions: {} };
+  }
+
+  search = () => {
+    let queryString = "http://sample.com?";
+    for (let [key, value] of Object.entries(this.state.conditions)) {
+      queryString += `${key}=${value}&`;
+    }
+    alert(queryString);
+  };
+
+  onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      conditions: Object.assign(this.state.conditions, {
+        [event.target.name]: event.target.value,
+      }),
+    });
+    console.log(this.state.conditions);
+  };
+
+  render() {
+    return (
+      <>
+        <h3>rest API search condition maker</h3>
+        <div>
+          <label>input1 : </label>
+          <input type="number" name="input1" onChange={this.onChange}></input>
+        </div>
+        <div>
+          <label>input2 : </label>
+          <input type="text" name="input2" onChange={this.onChange}></input>
+        </div>
+        <div>
+          <label>input3 : </label>
+          <input type="text" name="input3" onChange={this.onChange}></input>
+        </div>
+        <div>
+          <input type="button" value="search" onClick={this.search}></input>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
